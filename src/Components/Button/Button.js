b058fbbbd89f4b8788450ae2bae01e2f02f22aac
pawgog <p.gogola@interia.pl>
2020-02-07 11:01:08 +0100

    Separated button and changed background color.

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+const Button = ({ children, colorInput, ...props }) => {
+  console.log({colorInput});
+  
+  return (
+    <>
+      <button 
+        className={colorInput.fontColor === true ? styles.button__black : styles.button__white} 
+        style={{backgroundColor: colorInput.value}}
+        {...props}
+      >
+        {children}
+		  </button>
+    </>
+  )
+};
+
+export default Button;
--- src/Components/ColorList/ColorList.js
+++ src/Components/ColorList/ColorList.js
@@ -1,15 +1,21 @@
 import React, { useState, useRef } from 'react';
 import styles from './ColorList.module.scss';
+import Button from '../Button/Button'
 import { ColorFontVal } from '../../Func/Func'
 
 const ColorList = () => {
-  const [colorList, setColor] = useState([]);
+	const [colorList, setColor] = useState([]);
+	const [colorInput, setColorInput] = useState({
+		value: "#000000",
+		fontColor: false 
+	})
+	
   const colorRef = useRef(null)
   console.log(colorList);
 
 	const setColorValue = (val) => {
 		const colorFont = ColorFontVal(val.current.value);
-		
+
 		setColor([...colorList, 
 			{
 				value: val.current.value,
@@ -18,15 +24,25 @@ const ColorList = () => {
 		])
 	}
 
+	const setColorButton = (val) => {
+		console.log(val.current.value);
+		const colorFont = ColorFontVal(val.current.value);
+
+		setColorInput({
+				value: val.current.value,
+				fontColor: colorFont 
+			})
+	}
+
   return (
-	<div className={styles.Wrapper}>
-		<input ref={colorRef} type="color" name="color"></input>
-		<button onClick={() => setColorValue(colorRef)}>
+	<div className={styles.wrapper}>
+		<input className={styles.input} ref={colorRef} onChange={() => setColorButton(colorRef)} type="color" name="color"></input>
+		<Button colorInput={colorInput} onClick={() => setColorValue(colorRef)}>
 			Set Color Style
-		</button>
+		</Button>
 		{colorList.slice(0).slice(-5).map((item, index) => 
-			<div key={index} className={styles.Content} style={{backgroundColor: item.value}}>
-				<span className={item.fontColor ? styles.FontColor__black : styles.FontColor__white}>{item.value}</span>
+			<div key={index} className={styles.content} style={{backgroundColor: item.value}}>
+				<span className={item.fontColor ? styles.fontColor__black : styles.fontColor__white}>{item.value}</span>
 			</div>
 		)
 		}
